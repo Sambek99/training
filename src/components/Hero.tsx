1f3b98aa904ed63b7284c5c29e76dd856d76f226
Sambek99 <bgaona@espol.edu.ec>
2025-07-01 13:51:06 -0500

    fix6

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -56,6 +56,12 @@ const Hero = () => {
   return (
     <section
       id="hero"
+      className="relative
+                 min-h-[750px]
+                 sm:min-h-[700px]
+                 md:min-h-[800px]
+                 custom_lg:min-h-screen
+                 bg-gray-900 text-white flex items-center overflow-hidden"
     >
       <div className="absolute inset-0 z-0">
         <Carousel
